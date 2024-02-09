c488b4df83e3a3460f3e1b2a6d7655825c23c64a
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-09 18:26:04 +0300

    Update MainContextProvider.tsx

--- src/Context/MainContextProvider.tsx
+++ src/Context/MainContextProvider.tsx
@@ -1,22 +1,26 @@
-import MoneyContext from '../Context/MoneyContext'
-import ReceiptContext from './ReceiptContext';
-import { useState } from "react"
+import MoneyContext from "../Context/MoneyContext";
+import ReceiptContext from "./ReceiptContext";
+import { useState } from "react";
+import data from "../Data/Items";
 
-function MainContextProvider({ children }) { 
+function MainContextProvider({ children }) {
     const [currentMoney, setCurrentMoney] = useState(100000000000);
+    const [newData, setNewData] = useState(data);
 
-    const data = {
+    const money = {
         currentMoney,
-        setCurrentMoney
-    }
+        setCurrentMoney,
+        newData,
+        setNewData
+    };
 
     return (
-        <MoneyContext.Provider value={data}>
+        <MoneyContext.Provider value={money}>
             <ReceiptContext.Provider value={[]}>
                 {children}
             </ReceiptContext.Provider>
         </MoneyContext.Provider>
-    )
+    );
 }
 
 export default MainContextProvider;
