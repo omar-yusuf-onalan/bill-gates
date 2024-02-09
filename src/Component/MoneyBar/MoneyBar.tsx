5e35c99ff78b43a14b20ad722e9de03c0f1aba7c
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-09 10:48:44 +0300

    Create MoneyBar.tsx

---
+++ src/Component/MoneyBar/MoneyBar.tsx
@@ -0,0 +1,13 @@
+import './MoneyBar.style.css'
+
+function MoneyBar() {
+    return (
+        <>
+            <div className="money-bar-container">
+                <h2>$<span>100,000,000,000</span></h2>
+            </div>
+        </>
+    );
+}
+
+export default MoneyBar;
