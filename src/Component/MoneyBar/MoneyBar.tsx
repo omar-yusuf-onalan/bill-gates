ecd60d72e45fe4d3a0bbc54d39fcb1dac7780648
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-09 17:06:01 +0300

    Update MoneyBar.tsx

--- src/Component/MoneyBar/MoneyBar.tsx
+++ src/Component/MoneyBar/MoneyBar.tsx
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
 import './MoneyBar.style.css'
+import MoneyContext from '../../Context/MoneyContext';
+
+
 
 function MoneyBar() {
+    const { currentMoney, setCurrentMoney } = useContext(MoneyContext);
+
+
     return (
         <>
+    
             <div className="money-bar-container">
-                <h2>$<span>100,000,000,000</span></h2>
+                <h2>$<span>{currentMoney}</span></h2>
             </div>
         </>
     );
