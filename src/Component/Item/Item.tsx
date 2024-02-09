11dd69f372c21324a73fe201df3ab86901c70539
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-09 17:05:52 +0300

    Update Item.tsx

--- src/Component/Item/Item.tsx
+++ src/Component/Item/Item.tsx
@@ -1,6 +1,8 @@
-import './Item.style.css'
-import ActionButton from '../ActionButton/ActionButton'
-import ItemQuantityInput from '../ItemQuantityInput/ItemQuantityInput';
+import "./Item.style.css";
+import SellButton from "../SellButton/SellButton";
+import ItemQuantityInput from "../ItemQuantityInput/ItemQuantityInput";
+import BuyButton from "../BuyButton/BuyButton";
+import ItemLevelContextProvider from "../../Context/ItemLevelContextProvider";
 
 interface Prop {
     id: string;
@@ -11,21 +13,23 @@ interface Prop {
 
 function Item(prop: Prop) {
     return (
-        <>
-            <div className='container'>
-                <div className='item-description'>
-                    <div><img src={prop.img} alt={prop.name}/></div>
-                    <h2>{prop.name}</h2>
-                    <h4>${prop.price}</h4>
+        <div className="container">
+            <div className="item-description">
+                <div>
+                    <img src={prop.img} alt={prop.name} />
                 </div>
-
-                <div className='buy-sell'>
-                    <ActionButton buttonName={'Sell'}></ActionButton>
-                    <ItemQuantityInput></ItemQuantityInput>
-                    <ActionButton buttonName={'Buy'}></ActionButton>
-                </div>
+                <h2>{prop.name}</h2>
+                <h4>${prop.price}</h4>
             </div>
-        </>
+
+            <ItemLevelContextProvider>
+                <div className="buy-sell">
+                    <SellButton itemId={prop.id}></SellButton>
+                    <ItemQuantityInput></ItemQuantityInput>
+                    <BuyButton itemId={prop.id}></BuyButton>
+                </div>
+            </ItemLevelContextProvider>
+        </div>
     );
 }
 
