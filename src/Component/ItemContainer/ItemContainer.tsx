919a6bade9b51e0eb005a7ab84f4b5143e4f1f35
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-09 12:25:33 +0300

    Update ItemContainer.tsx

--- src/Component/ItemContainer/ItemContainer.tsx
+++ src/Component/ItemContainer/ItemContainer.tsx
@@ -1,13 +1,14 @@
-import './ItemContainer.style.css'
+import "./ItemContainer.style.css";
+import data from '../../Data/Items'
+import Item from "../Item/Item";
+
 
 function ItemContainer() {
-    return (
-        <>
-            <div className="item-container">
-            
-            </div>
-        </>
-    );
+    return <div className="item-container">
+        {data.map(item => {
+            return (<Item id={item.id} img={item.img} name={item.name} price={item.price} ></Item>)
+        })}
+    </div>;
 }
 
 export default ItemContainer;
