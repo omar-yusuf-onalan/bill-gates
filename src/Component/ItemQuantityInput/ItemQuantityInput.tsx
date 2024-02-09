1c5970dac22398ab61c6e673171224f2ada8dad1
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-09 12:25:11 +0300

    Create ItemQuantityInput.tsx

---
+++ src/Component/ItemQuantityInput/ItemQuantityInput.tsx
@@ -0,0 +1,11 @@
+import './ItemQuantityInput.style.css'
+
+function ItemQuantityInput() {
+    return (
+        <>
+        <input type="number" />
+        </>
+    );
+}
+
+export default ItemQuantityInput;
