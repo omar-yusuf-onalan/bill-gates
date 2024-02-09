64112c1d0984e4126bb55048c967cb0540567a49
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-09 17:06:40 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import "./App.css";
 import ItemContainer from "./Component/ItemContainer/ItemContainer.tsx";
 import MoneyBar from "./Component/MoneyBar/MoneyBar.tsx";
 import Profile from "./Component/Profile/Profile.tsx";
+import Receipt from "./Component/Receipt/Receipt.tsx";
+import MainContextProvider from "./Context/MainContextProvider.tsx";
 
 function App() {
     return (
         <>
-            <div className="app-container">
-                <Profile></Profile>
-                <MoneyBar></MoneyBar>
-                <ItemContainer></ItemContainer>
-            </div>
+            <MainContextProvider>
+                <div className="app-container">
+                    <Profile></Profile>
+                    <MoneyBar></MoneyBar>
+                    <ItemContainer></ItemContainer>
+                    <Receipt></Receipt>
+                </div>
+            </MainContextProvider>
         </>
     );
 }
