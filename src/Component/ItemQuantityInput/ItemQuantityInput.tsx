30c28161612f21b1590bf7d38d845dbf5dc68698
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-09 17:05:57 +0300

    Update ItemQuantityInput.tsx

--- src/Component/ItemQuantityInput/ItemQuantityInput.tsx
+++ src/Component/ItemQuantityInput/ItemQuantityInput.tsx
@@ -1,9 +1,23 @@
-import './ItemQuantityInput.style.css'
+import InputContext from "../../Context/InputContext";
+import "./ItemQuantityInput.style.css";
+import { useContext } from "react";
 
 function ItemQuantityInput() {
+    let { input, setInput } = useContext(InputContext);
+
+    const handleInput = (event) => {
+        console.log(event.target.value);
+        setInput((previousInput) => event.target.value);
+    };
+
     return (
         <>
-        <input className='input' type="number" />
+            <input
+                onChange={handleInput}
+                id="input"
+                className="input"
+                type="number"
+            />
         </>
     );
 }
