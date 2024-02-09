27de3d9070cb2000bd4b1248719854f33f7eb462
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-09 17:06:24 +0300

    Add bought field

--- src/Data/Items.tsx
+++ src/Data/Items.tsx
@@ -3,37 +3,43 @@ const data = [
         id: '1',
         img: 'src/assets/big-mac.jpg',
         name: 'Bic Mac',
-        price: 2
+        price: 2,
+        bought: 0
     },
     {
         id: '2',
         img: 'src/assets/flip-flops.jpg',
         name: 'Flip Flops',
-        price: 3
+        price: 3,
+        bought: 0
     },
     {
         id: '3',
         img: 'src/assets/coca-cola-pack.jpg',
         name: 'Coca-Cola Pack',
-        price: 5
+        price: 5,
+        bought: 0
     },
     {
         id: '4',
         img: 'src/assets/skyscraper.jpg',
         name: 'Skyscraper',
-        price: 850000000
+        price: 850000000,
+        bought: 0
     },
     {
         id: '5',
         img: 'src/assets/cruise-ship.jpg',
         name: 'Cruise Ship',
-        price: 930000000
+        price: 930000000,
+        bought: 0
     },
     {
         id: '6',
         img: 'src/assets/nba-team.jpg',
         name: 'NBA Team',
-        price: 2120000000
+        price: 2120000000,
+        bought: 0
     }
 ]
 
