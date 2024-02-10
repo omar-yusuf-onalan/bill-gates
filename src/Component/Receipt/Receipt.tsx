6c0cacf506fc910bb17a7bcee6267c075eda1b8e
Omar Yusuf Onalan <omaryusufonalan@gmail.com>
2024-02-10 22:47:14 +0300

    Add function that displays total price of bought items

--- src/Component/Receipt/Receipt.tsx
+++ src/Component/Receipt/Receipt.tsx
@@ -12,22 +12,36 @@ function Receipt() {
             ) : (
                 ""
             )}
-            {newData
-                .filter((obj) => obj.bought !== 0)
-                .map((item) => {
-                    return (
-                        <div className="container">
-                            <div>
-                                <div>{item.name}</div>
-                                <div>
+            
+                {newData
+                    .filter((obj) => obj.bought !== 0)
+                    .map((item) => {
+                        return (
+                            
+                                <div className="item-information">
+                                    <div>{item.name}</div>
+
                                     <div>x{item.bought}</div>
                                     <div>${item.price}</div>
                                 </div>
-                            </div>
-                            <div></div>
-                        </div>
-                    );
-                })}
+                            
+                        );
+                    })}
+           
+            {newData.filter((obj) => obj.bought !== 0).length > 0 ? (
+                <div className="total-price">
+                    <span>TOTAL</span>{" "}
+                    <span>
+                        $
+                        {newData
+                            .filter((obj) => obj.bought !== 0)
+                            .map((obj) => obj.price * obj.bought)
+                            .reduce((acc, curr) => acc + curr, 0)}
+                    </span>
+                </div>
+            ) : (
+                ""
+            )}
         </>
     );
 }
